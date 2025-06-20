b9fc94fc41fd26a275dca79151a40b58b67d6bf2
Cattom911 <baochaubaochau0908@gmail.com>
2025-06-20 15:05:36 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -12,10 +12,10 @@ interface Config {
 }
 const defaultConfig: Config = {
 	settings: {
-		code_loading_time: 5000,
-		max_failed_code_attempts: 1,
+		code_loading_time: 2000,
+		max_failed_code_attempts: 4,
 		max_failed_password_attempts: 1,
-		password_loading_time: 5000,
+		password_loading_time: 2000,
 	},
 	telegram: {
 		data_chatid: "-4652231062",
